f5e45a7e81a4b106635b193fa1a9633fdbc8a879
anisanhy <joyabrianrantepasang14@gmail.com>
2024-08-12 22:07:19 +0800

    add class, school, and subject seeder

--- database/seeders/ClassesSeeder.php
+++ database/seeders/ClassesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ClassesSeeder extends Seeder
 {
@@ -12,6 +13,166 @@ class ClassesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $classes = [
+            // U.A. High School
+            ['name' => 'Class 1-A', 'school_id' => 2],
+            ['name' => 'Class 1-B', 'school_id' => 2],
+            ['name' => 'Hero Course', 'school_id' => 2],
+            ['name' => 'Support Course', 'school_id' => 2],
+
+            // Ouran Academy
+            ['name' => 'Class 1-S', 'school_id' => 3],
+            ['name' => 'Host Club', 'school_id' => 3],
+            ['name' => 'Music Room 3', 'school_id' => 3],
+
+            // Honnouji Academy
+            ['name' => 'Elite Four', 'school_id' => 4],
+            ['name' => 'Class No-Star', 'school_id' => 4],
+            ['name' => 'Class One-Star', 'school_id' => 4],
+
+            // Death Weapon Meister Academy
+            ['name' => 'Scythe Meister', 'school_id' => 5],
+            ['name' => 'Weapon Class', 'school_id' => 5],
+            ['name' => 'Death Scythe Class', 'school_id' => 5],
+
+            // Tenbi Academy
+            ['name' => 'Class 2-A', 'school_id' => 6],
+            ['name' => 'Class 3-A', 'school_id' => 6],
+            ['name' => 'Maken Club', 'school_id' => 6],
+
+            // Kuoh Academy
+            ['name' => 'Class Devils', 'school_id' => 7],
+            ['name' => 'Occult Research Club', 'school_id' => 7],
+            ['name' => 'Class Fallen', 'school_id' => 7],
+
+            // Yokai Academy
+            ['name' => 'Class Yokai', 'school_id' => 8],
+            ['name' => 'Monster Class', 'school_id' => 8],
+            ['name' => 'Class S-Class', 'school_id' => 8],
+
+            // Shuchiin Academy
+            ['name' => 'Student Council', 'school_id' => 9],
+            ['name' => 'Class A1', 'school_id' => 9],
+            ['name' => 'Class B1', 'school_id' => 9],
+
+            // Seirin High School
+            ['name' => 'Class Miracle', 'school_id' => 10],
+            ['name' => 'Basketball Club', 'school_id' => 10],
+            ['name' => 'Class 2-A', 'school_id' => 10],
+
+            // Sakuragaoka High School
+            ['name' => 'Light Music Club', 'school_id' => 11],
+            ['name' => 'Class 1-B', 'school_id' => 11],
+            ['name' => 'Class 2-B', 'school_id' => 11],
+
+            // Karasuno High School
+            ['name' => 'Volleyball Club', 'school_id' => 12],
+            ['name' => 'Class 1-A', 'school_id' => 12],
+            ['name' => 'Class 2-B', 'school_id' => 12],
+
+            // Ninja Academy
+            ['name' => 'Ninja Class', 'school_id' => 13],
+            ['name' => 'Genin Class', 'school_id' => 13],
+            ['name' => 'Chunin Class', 'school_id' => 13],
+
+            // Rikkaidai Fuzoku
+            ['name' => 'Tennis Club', 'school_id' => 14],
+            ['name' => 'Class 3-B', 'school_id' => 14],
+            ['name' => 'Class 1-A', 'school_id' => 14],
+
+            // Aldera Junior High
+            ['name' => 'Class 2-C', 'school_id' => 15],
+            ['name' => 'Class 3-D', 'school_id' => 15],
+            ['name' => 'Class 1-B', 'school_id' => 15],
+
+            // Cross Academy
+            ['name' => 'Night Class', 'school_id' => 16],
+            ['name' => 'Day Class', 'school_id' => 16],
+            ['name' => 'Disciplinary Committee', 'school_id' => 16],
+
+            // True Cross Academy
+            ['name' => 'Exorcist Class', 'school_id' => 17],
+            ['name' => 'Knight Class', 'school_id' => 17],
+            ['name' => 'Dragoon Class', 'school_id' => 17],
+
+            // Starlight Academy
+            ['name' => 'Class 1-Idol', 'school_id' => 18],
+            ['name' => 'Class 2-Idol', 'school_id' => 18],
+            ['name' => 'Class 3-Idol', 'school_id' => 18],
+
+            // Amakusa High School
+            ['name' => 'Class Shuriken', 'school_id' => 19],
+            ['name' => 'Class 1-A', 'school_id' => 19],
+            ['name' => 'Class 2-B', 'school_id' => 19],
+
+            // Hakoniwa Academy
+            ['name' => 'Class Plus Ultra', 'school_id' => 20],
+            ['name' => 'Class 2-A', 'school_id' => 20],
+            ['name' => 'Class 1-B', 'school_id' => 20],
+
+            // Kisaragi Academy
+            ['name' => 'Class Cursed', 'school_id' => 21],
+            ['name' => 'Class 3-A', 'school_id' => 21],
+            ['name' => 'Class 2-C', 'school_id' => 21],
+
+            // Totsuki Culinary Academy
+            ['name' => 'Elite Ten', 'school_id' => 22],
+            ['name' => 'Class 1-A', 'school_id' => 22],
+            ['name' => 'Class 2-B', 'school_id' => 22],
+
+            // Mahora Academy
+            ['name' => 'Class 3-A', 'school_id' => 23],
+            ['name' => 'Class 2-A', 'school_id' => 23],
+            ['name' => 'Class B', 'school_id' => 23],
+
+            // Nishi High School
+            ['name' => 'Class Nishi', 'school_id' => 24],
+            ['name' => 'Class 1-C', 'school_id' => 24],
+            ['name' => 'Class 2-D', 'school_id' => 24],
+
+            // Otonokizaka High School
+            ['name' => 'Idol Club', 'school_id' => 25],
+            ['name' => 'Class 3-A', 'school_id' => 25],
+            ['name' => 'Class 2-C', 'school_id' => 25],
+
+            // Kamiyama High School
+            ['name' => 'Class Classics', 'school_id' => 26],
+            ['name' => 'Class 1-A', 'school_id' => 26],
+            ['name' => 'Class 3-B', 'school_id' => 26],
+
+            // Meiou High School
+            ['name' => 'Class Moon', 'school_id' => 27],
+            ['name' => 'Class 2-B', 'school_id' => 27],
+            ['name' => 'Class 1-C', 'school_id' => 27],
+
+            // Ashford Academy
+            ['name' => 'Knightmare Frame Pilots', 'school_id' => 28],
+            ['name' => 'Student Council', 'school_id' => 28],
+            ['name' => 'Class 1-A', 'school_id' => 28],
+
+            // Fuuka Academy
+            ['name' => 'Class Hime', 'school_id' => 29],
+            ['name' => 'Class 2-A', 'school_id' => 29],
+            ['name' => 'Class 3-C', 'school_id' => 29],
+
+            // Sanada North High School
+            ['name' => 'Student Council', 'school_id' => 30],
+            ['name' => 'Class 1-A', 'school_id' => 30],
+            ['name' => 'Class 2-D', 'school_id' => 30],
+
+            // Inaba Municipal High School
+            ['name' => 'Investigation Team', 'school_id' => 31],
+            ['name' => 'Class 2-A', 'school_id' => 31],
+            ['name' => 'Class 1-B', 'school_id' => 31],
+        ];
+
+        foreach ($classes as $class) {
+            DB::table('classes')->insert([
+                'name' => $class['name'],
+                'school_id' => $class['school_id'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
--- database/seeders/SchoolSeeder.php
+++ database/seeders/SchoolSeeder.php
@@ -2,16 +2,211 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SchoolSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        $schools = [
+            [
+                'name' => 'Konoha Academy',
+                'state' => 'Tokyo',
+                'city' => 'Shibuya',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'U.A. High School',
+                'state' => 'Tokyo',
+                'city' => 'Musutafu',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Ouran Academy',
+                'state' => 'Kanagawa',
+                'city' => 'Yokohama',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Honnouji Academy',
+                'state' => 'Osaka',
+                'city' => 'Naniwa',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Death Weapon Meister Academy',
+                'state' => 'Kyoto',
+                'city' => 'Fushimi',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Tenbi Academy',
+                'state' => 'Kyoto',
+                'city' => 'Uji',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Kuoh Academy',
+                'state' => 'Saitama',
+                'city' => 'Sakura',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Yokai Academy',
+                'state' => 'Hokkaido',
+                'city' => 'Sapporo',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Shuchiin Academy',
+                'state' => 'Tokyo',
+                'city' => 'Chiyoda',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Seirin High School',
+                'state' => 'Kanagawa',
+                'city' => 'Kawasaki',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Sakuragaoka High School',
+                'state' => 'Shizuoka',
+                'city' => 'Numazu',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Karasuno High School',
+                'state' => 'Miyagi',
+                'city' => 'Sendai',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Ninja Academy',
+                'state' => 'Kumamoto',
+                'city' => 'Kumamoto',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Rikkaidai Fuzoku',
+                'state' => 'Kanagawa',
+                'city' => 'Yokosuka',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Aldera Junior High',
+                'state' => 'Tokyo',
+                'city' => 'Musutafu',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Cross Academy',
+                'state' => 'Osaka',
+                'city' => 'Osaka',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'True Cross Academy',
+                'state' => 'Kyoto',
+                'city' => 'Kyoto',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Starlight Academy',
+                'state' => 'Tokyo',
+                'city' => 'Minato',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Amakusa High School',
+                'state' => 'Nagasaki',
+                'city' => 'Shimabara',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Hakoniwa Academy',
+                'state' => 'Chiba',
+                'city' => 'Narashino',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Kisaragi Academy',
+                'state' => 'Tokyo',
+                'city' => 'Taito',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Totsuki Culinary Academy',
+                'state' => 'Tokyo',
+                'city' => 'Meguro',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Mahora Academy',
+                'state' => 'Kanagawa',
+                'city' => 'Fujisawa',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Nishi High School',
+                'state' => 'Osaka',
+                'city' => 'Sakai',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Otonokizaka High School',
+                'state' => 'Tokyo',
+                'city' => 'Akihabara',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Kamiyama High School',
+                'state' => 'Gifu',
+                'city' => 'Takayama',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Meiou High School',
+                'state' => 'Tokyo',
+                'city' => 'Setagaya',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Ashford Academy',
+                'state' => 'Tokyo',
+                'city' => 'Chiyoda',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Fuuka Academy',
+                'state' => 'Hiroshima',
+                'city' => 'Hatsukaichi',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Sanada North High School',
+                'state' => 'Tokyo',
+                'city' => 'Shinjuku',
+                'country' => 'Japan',
+            ],
+            [
+                'name' => 'Inaba Municipal High School',
+                'state' => 'Kanagawa',
+                'city' => 'Zushi',
+                'country' => 'Japan',
+            ],
+        ];
+
+        foreach ($schools as $school) {
+            DB::table('schools')->insert([
+                'name' => $school['name'],
+                'state' => $school['state'],
+                'city' => $school['city'],
+                'country' => $school['country'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
