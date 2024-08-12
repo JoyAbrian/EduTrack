fe3bf46ed5dcd0086c01f8b3c08714a48a4c3dc0
anisanhy <joyabrianrantepasang14@gmail.com>
2024-08-12 13:41:36 +0800

    add active dashboard

--- resources/views/partials/Dashboard/sidebar.blade.php
+++ resources/views/partials/Dashboard/sidebar.blade.php
@@ -6,22 +6,22 @@
     <hr class="border border-gray-400 h-0.5 my-5">
     <ul class="px-6">
         <li class="mb-4">
-            <div class="text-white font-Kumbh font-semibold text-lg flex p-2 hover:bg-blue-800 rounded">
+            <div class="text-white font-Kumbh font-semibold text-lg flex p-2 rounded {{ Request::is('dashboard') ? 'bg-blue-500' : 'hover:bg-blue-800' }}">
                 <a href="/dashboard/" class="items-center">
                     <i class="bi bi-grid mr-3"></i> Dashboard
                 </a>
             </div>
         </li>
         <li class="mb-4">
-            <div class="text-white font-Kumbh font-semibold text-lg flex p-2 hover:bg-blue-800 rounded">
-                <a href="/dashboard/" class="items-center">
+            <div class="text-white font-Kumbh font-semibold text-lg flex p-2 hover:bg-blue-800 rounded {{ Request::is('dashboard/teacher') ? 'bg-blue-800' : '' }}">
+                <a href="/dashboard/teacher" class="items-center">
                     <i class="bi-person-fill mr-3"></i> Teacher
                 </a>
             </div>
         </li>
         <li class="mb-4">
-            <div class="text-white font-Kumbh font-semibold text-lg flex p-2 hover:bg-blue-800 rounded">
-                <a href="/dashboard/" class="items-center">
+            <div class="text-white font-Kumbh font-semibold text-lg flex p-2 hover:bg-blue-800 rounded {{ Request::is('dashboard/students') ? 'bg-blue-800' : '' }}">
+                <a href="/dashboard/students" class="items-center">
                     <i class="bi-person-check mr-3"></i> Students
                 </a>
             </div>
